a638b46e4656a19d73e5aa1e8d032b98f9e11e1d
vasquezdennisalon <vasquezdennisan@gmail.com>
2020-04-17 07:26:31 -0600

    Creando demo con la libreria

---
+++ src/app/home/home.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { IonicModule } from '@ionic/angular';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+
+import { HomePage } from './home.page';
+import { FivAppBarModule } from '@fivethree/core';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    RouterModule.forChild([
+      {
+        path: '',
+        component: HomePage
+      }
+    ]),
+    FivAppBarModule
+  ],
+  declarations: [HomePage]
+})
+export class HomePageModule {}
